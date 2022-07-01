f6132be65225af89cc8af05956af3aa276c4628c
KALININ ALEXEY <56075795+lelikone777@users.noreply.github.com>
2022-07-02 01:27:29 +0300

    mark & baloon

--- script.js
+++ script.js
@@ -6,6 +6,33 @@ function init() {
         zoom: 17
     });
 
+    let placemark = new ymaps.Placemark(center, {
+        balloonContentHeader: 'Хедер балуна',
+        balloonContentBody: 'Боди балуна',
+        balloonContentFooter: 'Подвал',
+    }, {
+        iconLayout: 'default#image',
+        iconImageHref: 'marker.png',
+        iconImageSize: [50, 50],
+        iconImageOffset: [-19, -44]
+    });
+
+    let placemark1 = new ymaps.Placemark(center, {
+        balloonContent: `
+			<div class="balloon">
+				<div class="balloon__address">г. Париж</div>
+				<div class="balloon__contacts">
+					<a href="tel:+7999999999">+7999999999</a>
+				</div>
+			</div>
+		`
+    }, {
+        iconLayout: 'default#image',
+        iconImageHref: 'marker.png',
+        iconImageSize: [40, 40],
+        iconImageOffset: [-19, -44]
+    });
+
     map.controls.remove('geolocationControl'); // удаляем геолокацию
     map.controls.remove('searchControl'); // удаляем поиск
     map.controls.remove('trafficControl'); // удаляем контроль трафика
@@ -14,6 +41,13 @@ function init() {
     /* map.controls.remove('zoomControl');  // удаляем контрол зуммирования */
     map.controls.remove('rulerControl'); // удаляем контрол правил
     map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+
+
+    // map.geoObjects.add(placemark);
+
+    map.geoObjects.add(placemark1);
+
+    placemark1.balloon.open();
 }
 
 ymaps.ready(init);
